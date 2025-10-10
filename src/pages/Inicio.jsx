ad9afd0ce8b4495d9b2a178701521c550a908e82
Sergio-Tolaba <tolser2019@gmail.com>
2025-10-10 07:40:22 -0300

    useState en el padre Inicio, hijos sin cambios - agrego producto: "ñoquis"

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,10 +1,17 @@
-import ProductList from "../components/ProductList";
 
+import ProductList from "../components/ProductList";
+import {useState} from 'react'
 const Inicio = ()=>{
-    const productos = ['Pizza', 'Arroz con huevo', 'Matambre']
+    //const productos = ['Pizza', 'Arroz con huevo', 'Matambre']
+    const [productos, setProductos] = useState(['Pizza', 'Arroz con huevo', 'Matambre'])
+    const agregarProducto = ()=>{
+        setProductos([...productos,"ñoquis"]) //actualiza estado si recargar la SPA
+    }
     return(
         <div>
             <h2>Comidas deliciosas</h2>
+            <button onClick={agregarProducto}>Agregar producto</button>
+            {/* estado productos pasa como props */}
             <ProductList items={productos} />
         </div>
 
