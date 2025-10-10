3506a1bb658f74bfd1b58a6518b7084b316e73c9
Sergio-Tolaba <tolser2019@gmail.com>
2025-10-10 10:19:27 -0300

    formulario con imput para nuevo producto

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,23 @@ import './App.css';
 import Inicio from './pages/Inicio'
 
 function App() {
-  const comidas = [
-{ id: 1, name: "Pizza", price: 12.5 },
-{ id: 2, name: "Hamburguesa", price: 10.0 },
-{ id: 3, name: "Empanadas", price: 8.75 },
-{ id: 4, name: "Milanesa", price: 11.25 },
-];
+//   const comidas = [
+// { id: 1, name: "Pizza", price: 12.5 },
+// { id: 2, name: "Hamburguesa", price: 10.0 },
+// { id: 3, name: "Empanadas", price: 8.75 },
+// { id: 4, name: "Milanesa", price: 11.25 },
+// ];
   return (
     <>
       <h1>St Gourmet</h1>
-      <ul>
+      {/* <ul>
         {comidas.map((c)=>(
           <li key={c.id}>
             <h2>{c.name}</h2>
             <p>${c.price} </p>
           </li>
         ))}
-      </ul>
+      </ul> */}
       <Inicio />
     </>
   );
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,8 +4,16 @@ import {useState} from 'react'
 const Inicio = ()=>{
     //const productos = ['Pizza', 'Arroz con huevo', 'Matambre']
     const [productos, setProductos] = useState(['Pizza', 'Arroz con huevo', 'Matambre'])
-    const agregarProducto = ()=>{
-        setProductos([...productos,"ñoquis"]) //actualiza estado si recargar la SPA
+    const [nuevoProducto, setNuevoProducto] = useState("");
+    // const agregarProducto = ()=>{
+    //     setProductos([...productos,"ñoquis"]) //actualiza estado si recargar la SPA
+    // }
+    const agregarProducto = (e) => {
+        e.preventDefault(); 
+        if (nuevoProducto.trim() !== "") {
+        setProductos([...productos, nuevoProducto]);
+        setNuevoProducto("");
+        }
     }
     const eliminarProducto = (nombre)=>{
     setProductos(productos.filter((item)=> item !==nombre))
@@ -13,7 +21,15 @@ const Inicio = ()=>{
     return(
         <div>
             <h2>Comidas deliciosas</h2>
-            <button onClick={agregarProducto}>Agregar producto</button>
+            <form onSubmit={agregarProducto}>
+                <input
+                    type="text"
+                    placeholder="Escribí un producto"
+                    value={nuevoProducto}
+                    onChange={(e) => setNuevoProducto(e.target.value)}
+                />
+                <button onClick={agregarProducto}>Agregar producto</button>
+            </form>
             {/* estado productos pasa como props */}
             {/* funcion eliminarProducto pasa como props| */}
             <ProductList items={productos} onDelete={eliminarProducto} />
