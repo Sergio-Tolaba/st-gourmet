aef6baf0484d6cc2f5906c5fbc86382435eee224
Sergio-Tolaba <tolser2019@gmail.com>
2025-11-12 13:41:03 -0300

    mejorando detalle de productos en el celular, css

---
+++ src/components/Carrito - copia.jsx
@@ -0,0 +1,55 @@
+import { Link } from 'react-router-dom';
+import { useAuthContext } from '../context/AuthContext';
+import { useContext } from 'react';
+import './Carrito.css';
+import {CarritoContext} from '../context/CarritoContext'
+const Carrito = () => {
+  //Uso del Contexto
+  const { usuario } = useAuthContext();
+  const {carrito,eliminarProducto,vaciarCarrito, cambiarCantidad }=useContext(CarritoContext)
+  const total = carrito.reduce((acc, item) => acc + item.precio * (item.cantidad || 1), 0);
+  return (
+    <div className="carrito-container">
+      <h2>🛒 Tu Carrito</h2>
+          <p>Usuario: {usuario}</p>
+      
+          <button onClick={vaciarCarrito} className='btn-vaciar' >Vaciar carrito</button>
+          <button className='btn-pagar' >Ir a pagar</button>
+
+      {(!carrito || carrito.length === 0) ? (
+        <p className="carrito-vacio">El carrito está vacío.</p>
+      ) : (
+        <ul className="carrito-lista">
+          {carrito.map((item, index) => (
+            <li key={index} className="carrito-item">
+              <img src={item.imagen} alt={item.nombre} />
+              <div className="info">
+                <h4>{item.nombre}</h4>
+                <p>Precio: ${item.precio}</p>
+                 <div>
+                  <button onClick={()=>cambiarCantidad(item.nombre, -1)}>-</button>
+                   <samp>Cantidad: {item.cantidad || 1}</samp>
+                   <button onClick={()=>cambiarCantidad(item.nombre, +1)}>+</button>
+                 </div>
+
+                
+                <p>Subtotal: ${item.precio * (item.cantidad || 1)}</p>
+
+              </div>
+              <button className="btn-eliminar" onClick={() => eliminarProducto(index)}>
+                Eliminar
+              </button>
+
+            </li>
+          ))}
+        </ul>
+      )}
+      {carrito.length > 0 && (
+        <h3>Total: ${total}</h3>
+      )}
+    </div>
+  );
+};
+
+export default Carrito;
+
